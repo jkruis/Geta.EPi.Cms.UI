a986d91f09788814e2ef303193f6891df6dffa73
Mattias Olsson <mattias@geta.no>
2015-04-24 14:37:43 +0200

    First commit.

---
+++ src/ClientResources/Scripts/editors/FontAwesomeAutoSuggestEditor.js
@@ -0,0 +1,20 @@
+﻿define([
+    "dojo/_base/declare",
+    "geta-epi-cms/editors/_FontAwesomeMixin",
+    "epi/shell/form/AutoCompleteSelectionEditor"
+],
+function (
+    declare,
+    _FontAwesomeMixin,
+    AutoCompleteSelectionEditor
+) {
+    return declare("geta-epi-cms.editors.FontAwesomeAutoSuggestEditor", [_FontAwesomeMixin, AutoCompleteSelectionEditor], {
+        labelAttr: "label",
+        labelType: "html",
+
+        postCreate: function() {
+            this.inherited(arguments);
+            this.store = this._createMemoryStore();
+        }
+    });
+});
---
+++ src/Configuration/GetaUIHint.cs
@@ -0,0 +1,7 @@
+﻿namespace Geta.EPi.Cms.UI.Configuration
+{
+    public class GetaUIHint
+    {
+        public const string FontAwesomeIcon = "FontAwesomeIcon";
+    }
+}
