b4351ea6f53f75925a2fec7c72ab8dc3e596c13f
Mattias Olsson <mattias@geta.no>
2015-05-05 21:49:49 +0200

    Added editor descriptor and attribute for CategoryList to support selection based on configured root category name or id.

---
+++ src/EditorDescriptors/CategoryListEditorDescriptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Cms.Shell.UI.ObjectEditing.EditorDescriptors;
+using EPiServer.Core;
+using EPiServer.Shell.ObjectEditing;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+using Geta.EPi.Cms.UI.ObjectEditing;
+
+namespace Geta.EPi.Cms.UI.EditorDescriptors
+{
+    [EditorDescriptorRegistration(TargetType = typeof(CategoryList), EditorDescriptorBehavior = EditorDescriptorBehavior.OverrideDefault)]
+    public class CustomCategoryListEditorDescriptor : CategoryListEditorDescriptor
+    {
+        public override void ModifyMetadata(ExtendedMetadata metadata, IEnumerable<Attribute> attributes)
+        {
+            base.ModifyMetadata(metadata, attributes);
+            var categorySelectionAttribute = attributes.OfType<CategorySelectionAttribute>().FirstOrDefault();
+
+            if (categorySelectionAttribute != null)
+            {
+                metadata.EditorConfiguration["multiple"] = categorySelectionAttribute.Multiple;
+                metadata.EditorConfiguration["root"] = categorySelectionAttribute.GetRootCategoryId();
+            }
+        }
+    }
+}
---
+++ src/ObjectEditing/CategorySelectionAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.Mvc;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Shell.ObjectEditing;
+
+namespace Geta.EPi.Cms.UI.ObjectEditing
+{
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class CategorySelectionAttribute : Attribute
+{
+    public bool Multiple { get; set; }
+    public int RootCategoryId { get; set; }
+    public string RootCategoryName { get; set; }
+
+    public CategorySelectionAttribute()
+    {
+        Multiple = true;
+    }
+
+    public int GetRootCategoryId()
+    {
+        if (RootCategoryId > 0)
+        {
+            return RootCategoryId;
+        }
+
+        if (!string.IsNullOrEmpty(RootCategoryName))
+        {
+            var category = Category.Find(RootCategoryName);
+
+            if (category != null)
+            {
+                return category.ID;
+            }
+        }
+
+        return Category.GetRoot().ID;
+    }
+}
+}
